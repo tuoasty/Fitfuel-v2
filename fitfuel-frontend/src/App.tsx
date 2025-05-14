8e9bc2052cc5e7ab56f88e07b7d4e5a09cf918fe
tuoasty <tuoasty@gmail.com>
2025-05-14 08:36:08 +0700

    Shadcn frontend

---
+++ fitfuel-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import {Button} from "./components/ui/button.tsx";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-orange-500">Get Started</Button>
+    </>
+  )
+}
+
+export default App
