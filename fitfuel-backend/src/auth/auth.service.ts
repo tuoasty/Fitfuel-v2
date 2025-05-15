33ab17816c3bbe073ad449fc40c59adb28be24b7
tuoasty <tuoasty@gmail.com>
2025-05-15 08:16:30 +0700

    Feat : JWT Key generation

--- fitfuel-backend/src/auth/auth.service.ts
+++ fitfuel-backend/src/auth/auth.service.ts
@@ -1,15 +1,18 @@
 import {Injectable, UnauthorizedException} from '@nestjs/common';
 import {UsersService} from "../users/users.service";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
-    async signIn(email:string, pass:string) :Promise<any>{
+    constructor(private usersService: UsersService, private jwtService: JwtService) {}
+    async signIn(email:string, pass:string) :Promise<{access_token:string}>{
         const user = await this.usersService.findOne(email);
         if(user?.password !== pass){
             throw new UnauthorizedException();
         }
-        const {password, ...result} = user;
-        return result;
+        const payload = {sub: user.id, email:user.email};
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        }
     }
 }
