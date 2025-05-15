2cbab72b6aed6cc17f090b1ef3ef4b281d3713d7
tuoasty <tuoasty@gmail.com>
2025-05-15 11:28:50 +0700

    Feat : Public decorator for non authenticated functions

--- fitfuel-backend/src/auth/auth.controller.ts
+++ fitfuel-backend/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, HttpCode, HttpStatus, Post, Res} from '@nestjs/common'
 import {AuthService} from "./auth.service";
 import {IsEmail, IsNotEmpty} from "class-validator";
 import {Response} from "express";
+import {Public} from "../utils/decorator";
 
 class SignInDto {
     @IsEmail()
@@ -16,6 +17,7 @@ export class AuthController {
     constructor(private authService: AuthService){}
 
     @HttpCode(HttpStatus.OK)
+    @Public()
     @Post("login")
     signIn(@Body() signInDto:SignInDto,
            @Res() res:Response){
--- fitfuel-backend/src/auth/auth.guard.ts
+++ fitfuel-backend/src/auth/auth.guard.ts
@@ -2,26 +2,35 @@ import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "
 import {JwtService} from "@nestjs/jwt";
 import {Request} from "express";
 import {jwtConstants} from "./constants";
+import {Reflector} from "@nestjs/core";
+import {IS_PUBLIC_KEY} from "../utils/decorator";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private jwtService: JwtService) {
+    constructor(private jwtService: JwtService, private reflector: Reflector) {
     }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if(isPublic){
+            return true;
+        }
+
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromCookie(request);
         if (!token) {
             throw new UnauthorizedException();
         }
         try {
-            const payload = await this.jwtService.verifyAsync(
+            request['user'] = await this.jwtService.verifyAsync(
                 token,
                 {
                     secret: jwtConstants.secret
                 }
             );
-            request['user'] = payload;
         } catch {
             throw new UnauthorizedException();
         }
