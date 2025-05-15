b2c540eedb35dfef3bfc237593aa5f285895f0fc
tuoasty <tuoasty@gmail.com>
2025-05-15 10:56:17 +0700

    Feat : Global authentication guard

--- fitfuel-backend/src/app.module.ts
+++ fitfuel-backend/src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import {APP_GUARD} from "@nestjs/core";
+import {AuthGuard} from "./auth/auth.guard";
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: AuthGuard,
+  }],
 })
 export class AppModule {}
