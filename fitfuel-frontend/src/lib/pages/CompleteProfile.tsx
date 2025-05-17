49ad3bda17f474ede08b3fa332c6eab19c329754
tuoasty <tuoasty@gmail.com>
2025-05-17 16:22:59 +0700

    docs : added todo

--- fitfuel-frontend/src/lib/pages/CompleteProfile.tsx
+++ fitfuel-frontend/src/lib/pages/CompleteProfile.tsx
@@ -1,6 +1,7 @@
 export default function CompleteProfile() {
     return (
         <main>
+            {/*Todo -> Create a second register page for inputting profile data, such as weight, height, date of birth, and also diet and activty enum (Check schema.prisma)*/}
             Complete Profile
         </main>
     )
