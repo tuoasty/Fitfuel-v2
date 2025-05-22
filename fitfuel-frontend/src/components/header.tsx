ef8424bb87e36f8fbb4e263c54a6be91488b9a6b
tuoasty <tuoasty@gmail.com>
2025-05-22 15:50:57 +0700

    Style : Added shadows

--- fitfuel-frontend/src/components/header.tsx
+++ fitfuel-frontend/src/components/header.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function FitfuelHeader(p:Props){
     return (
-        <header className="sticky top-0 z-50 bg-background p-4 flex justify-between items-center">
+        <header className="sticky top-0 z-50 bg-background p-4 flex justify-between items-center shadow-md">
             <h1 className="text-xl font-bold text-primary">FitFuel</h1>
             <button onClick={p.toggleSidebar} className="p-1 cursor-pointer">
                 <Menu className="h-6 w-6 text-primary" />
