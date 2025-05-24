48e587648b1aa38806fb9c61284ab1380c7a54f8
ococtata <142913244+ococtata@users.noreply.github.com>
2025-05-24 10:28:35 +0700

    Fix: importing SupabaseModule

--- fitfuel-backend/src/article/article.module.ts
+++ fitfuel-backend/src/article/article.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ArticleController } from './article.controller';
+import { SupabaseModule } from 'src/supabase/supabase.module';
 
 @Module({
+  imports: [PrismaModule, SupabaseModule],
   providers: [ArticleService],
+  exports: [ArticleService],
+  controllers: [ArticleController],
 })
 export class ArticleModule {}
