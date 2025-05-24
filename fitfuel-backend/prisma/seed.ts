652974726c0ae5463505ace01dc552e380f5c21a
ococtata <142913244+ococtata@users.noreply.github.com>
2025-05-24 10:35:17 +0700

    Fix: realistic height

--- fitfuel-backend/prisma/seed.ts
+++ fitfuel-backend/prisma/seed.ts
@@ -13,9 +13,9 @@ async function main(){
 
     await prisma.profile.createMany({
         data: [
-            {id:"b1743b42-067d-4e8a-8576-aeb6698b7977", userId:"1bfbc920-1517-4827-bf2f-622e828c387f", weight:50, height:50, dateOfBirth:new Date(Date.now()),
+            {id:"b1743b42-067d-4e8a-8576-aeb6698b7977", userId:"1bfbc920-1517-4827-bf2f-622e828c387f", weight:50, height:173, dateOfBirth:new Date(Date.now()),
             picture_url:"https://nkwfrmiuazvqdmfktfka.supabase.co/storage/v1/object/public/public-bucket/profile-picture/1bfbc920-1517-4827-bf2f-622e828c387f/9lskx5.jpg"},
-            {id:"61c15379-348a-43e9-b6b3-8a93967f8ce1", userId:"dc8b5570-7735-4184-952a-27aa31cd8d90", weight:50, height:50, dateOfBirth:new Date(Date.now()),
+            {id:"61c15379-348a-43e9-b6b3-8a93967f8ce1", userId:"dc8b5570-7735-4184-952a-27aa31cd8d90", weight:50, height:175, dateOfBirth:new Date(Date.now()),
                 picture_url:"https://nkwfrmiuazvqdmfktfka.supabase.co/storage/v1/object/public/public-bucket/profile-picture/dc8b5570-7735-4184-952a-27aa31cd8d90/433727.jpg"}
         ]
     })
